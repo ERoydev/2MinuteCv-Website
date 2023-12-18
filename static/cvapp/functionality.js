9a7aaf56cc8c0c5866652bc16761c4c3ef6d0e8e
Emil Roydev <e.roydev@gmail.com>
2023-12-18 11:45:59 -0800

    create cv From logic applied

--- static/cvapp/functionality.js
+++ static/cvapp/functionality.js
@@ -1,3 +1,9 @@
+const allTrashButtons = document.querySelectorAll('.trash-btn');
+
+for(let btn of allTrashButtons) {
+    btn.addEventListener('click', deleteField)
+}
+
 
 function experienceField(id_v) {
 
@@ -17,4 +23,24 @@ function experienceField(id_v) {
     fieldBoxElement.appendChild(fieldBox);
     fieldBox.style.display='block';
     fieldBoxElement.value=true;
+}
+
+function educationField(id_v) {
+    const Boxes = Array.from(document.querySelectorAll('.education-box'));
+    const currentBox = Boxes[id_v];
+    const fieldBoxElement = currentBox.children[1];
+
+    if(currentBox.children[1].value) {
+        fieldBoxElement.innerHTML = '';
+        fieldBoxElement.value=false;
+        return;
+    }
+
+    const experienceContainer = document.querySelector('.education-container');
+    const fieldBox = experienceContainer.cloneNode(true);
+
+    fieldBoxElement.appendChild(fieldBox);
+    fieldBox.style.display='block';
+    fieldBoxElement.value=true;
+
 }
