679091337afb54df03f27e3f38de1f1ea4577aba
Emil Roydev <e.roydev@gmail.com>
2023-12-29 23:56:51 -0800

    implemented download PDF logic but not working correctly

--- static/cvapp/functionality.js
+++ static/cvapp/functionality.js
@@ -11,36 +11,28 @@ async function loadTemplateOnHalf() {
 
 
 
-function generate(template) {
-  let btn = document.querySelector('#download');
-  btn.addEventListener('click', downloadPDF)
+async function downloadPDF(e) {
+document.body.style.zoom = '67%';
 
-  async function downloadPDF(e) {
-    document.body.style.zoom = '67%';
+// Downloading
+let downloading = document.querySelector('.template-container');
+let doc = new jsPDF(1, "pt");
 
-    let downloadButton = document.querySelector('#download');
-    downloadButton.textContent = "Currently downloading...."
+await html2canvas(downloading, {
+    allowTaint: true,
+    useCORS: true,
+    width: 1175,
+    height: 1320,
+}).then((canvas) => {
 
-    // Downloading
-    let downloading = document.querySelector('.template-container');
-    let doc = new jsPDF(1, "pt");
+    // Canvas (Convert to PNG)
+    doc.addImage(canvas.toDataURL("image/png"), 'PNG', 0, 0, 700, 842);
+})
 
-    await html2canvas(downloading, {
-        allowTaint: true,
-        useCORS: true,
-        width: 1175,
-        height: 1320,
-    }).then((canvas) => {
-
-        // Canvas (Convert to PNG)
-        doc.addImage(canvas.toDataURL("image/png"), 'PNG', 0, 0, 700, 842);
-    })
-
-    document.body.style.zoom = '100%';
-    doc.save("Document.pdf");
-    btn.textContent = "Print And Generate PDF";
-  }
+document.body.style.zoom = '100%';
+doc.save("Document.pdf");
 }
+
 
 
 function experienceField(id_v) {
